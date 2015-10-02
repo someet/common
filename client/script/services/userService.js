2e50411432554db5d6d68e331e5a34a0649cf2ed
langshuang <409909041@qq.com>
2015-10-02 18:46:02 +0800

    添加user的service

--- client/script/services/userService.js
+++ client/script/services/userService.js
@@ -1,3 +1,10 @@
-/**
- * Created by langshuang997 on 15/10/2.
- */
+angular.module('backendServices')
+  .factory('$userManage', ['$http', '$q', '$rootScope', function($http, $q, $rootScope) {
+    return {
+      add: function(newUser) {
+        return $http.post('/user/add', newUser).then(function(data) {
+          alert("添加成功");
+        });
+      }
+    };
+  }]);
