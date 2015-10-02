e5b16b086633d5268321fcfef2d1b38f11746130
langshuang <409909041@qq.com>
2015-10-02 18:45:34 +0800

    添加user的添加方法

--- client/script/controllers/UserCtrl.js
+++ client/script/controllers/UserCtrl.js
@@ -1,7 +1,39 @@
 angular.module('controllers')
-.controller('DashboardCtrl', ['$scope', function($scope){
+.controller('UserAddCtrl', ['$scope', '$userManage', function($scope, $userManage){
+  $scope.user = {
+    username: "langshuang",
+    password: "",
+    password1: "",
+    rule: ""
+  };
+  $scope.checkUserResult = '';
 
-  $scope.clickMe = function() {
-    alert('clicked!');
+  $scope.createUser = function() {
+
+    var newUser = {
+      username: $scope.user.username,
+      password: $scope.user.password,
+      rule: $scope.user.rule
+    };
+
+    if($scope.checkUserResult == ''){
+      $userManage.add(newUser).then(function() {
+        $location.path('/user');
+      }, function(err) {
+        $scope.syncing = false;
+      });
+    } else {
+      alert($scope.checkUserResult);
+    }
+  }
+
+
+  function checkUser(){
+    if($scope.user.password == ""){
+      $scope.checkUserResult = "密码不能为空！";
+    }
+    if($scope.user.password != $scope.user.password1){
+      $scope.checkUserResult = "两次密码不一致！";
+    }
   }
 }]);
