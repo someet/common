2e1941fb98ab2b48fa8dec4e2a486e137c65c4e4
dcb9 <jt1dcb@163.com>
2015-10-03 10:01:50 +0800

    add PageName feature

--- client/script/controllers/DashboardCtrl.js
+++ client/script/controllers/DashboardCtrl.js
@@ -1,5 +1,7 @@
 angular.module('controllers')
 .controller('DashboardCtrl', ['$scope', function($scope){
+  $scope.$parent.pageName = 'Dashboard';
+
   $scope.clickMe = function() {
     alert('clicked!');
   }
--- views/site/index.php
+++ views/site/index.php
@@ -12,17 +12,18 @@ use yii\helpers\Html;
   <body layout="column" ng-controller="MainCtrl">
     <md-toolbar layout="row">
       <div class="md-toolbar-tools">
-        <md-button ng-click="toggleSidenav('left')" hide-gt-sm class="md-icon-button">
+        <md-button hide-lg ng-click="toggleSidenav('left')">
           <md-icon aria-label="Menu" md-svg-icon="https://s3-us-west-2.amazonaws.com/s.cdpn.io/68133/menu.svg"></md-icon>
+          <h1>Someet Backend</h1>
         </md-button>
-        <h1>Someet Backend</h1>
+        <h3>{{pageName}}</h3>
       </div>
     </md-toolbar>
     <div layout="row" flex>
         <md-sidenav layout="column"
           class="md-sidenav-left md-whiteframe-z2"
           md-component-id="left"
-          md-is-locked-open="$mdMedia('gt-sm')">
+          md-is-locked-open="$mdMedia('lg')">
           <md-list>
             <md-list-item>
               <a href="/#/activity-type">活动类型管理</a>
