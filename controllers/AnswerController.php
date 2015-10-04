68c1890bd8ad248a8d0db5e2b035ceb1bc6648bf
langshuang <409909041@qq.com>
2015-10-04 20:02:20 +0800

    添加answer的服务端Controller

--- controllers/AnswerController.php
+++ controllers/AnswerController.php
@@ -3,8 +3,9 @@
 namespace app\controllers;
 
 use app\components\DataValidationFailedException;
+use app\models\Answer;
+use app\models\AnswerItem;
 use app\models\Question;
-use app\models\QuestionItem;
 use Yii;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
@@ -12,7 +13,7 @@ use yii\web\NotFoundHttpException;
 use yii\web\Response;
 use yii\web\ServerErrorHttpException;
 
-class QuestionController extends Controller
+class AnswerController extends Controller
 {
 
     /**
@@ -46,56 +47,35 @@ class QuestionController extends Controller
         ];
     }
 
-    /**
-     * 添加一个报名表单
-     *
-     * POST 请求 /question/create
-     *
-     * ~~~
-     * {
-     *   "q1": <string: 问题1>,
-     * }
-     * ~~~
-     *
-     * @return  array
-     *
-     * 失败
-     *
-     * ~~~
-     * {
-     * "success": "0",
-     * "errmsg": "名称长度不得超过255个字符",
-     * "status_code": 422
-     * }
-     * ~~~
-     *
-     * 成功
-     *
-     * {
-     * "success": "1",
-     * "data": {
-     *   "id": 10,
-     *   "name": "户外",
-     *   "displayorder": 99,
-     *   "status": 10
-     * },
-     * "status_code": 200
-     * }
-     *
-     * @throws DataValidationFailedException
-     * @throws ServerErrorHttpException
-     */
+
     public function actionCreate()
     {
-        $request = Yii::$app->getRequest();
-        $response = Yii::$app->getResponse();
-        $response->format = Response::FORMAT_JSON;
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $post = Yii::$app->request->post();
+        $data = ['question_id' => $post['question_id'], 'activity_id' => $post['activity_id']];
+        $answerList = $post['answerList'];
 
-        $data = $request->post();
-        $model = new Question;
-
+        $model = new Answer();
         if ($model->load($data, '') && $model->save()) {
-            return Question::findOne($model->id);
+            if (!empty($answerList)) {
+                foreach ($answerList as $answer) {
+                    $answerModel = new AnswerItem();
+                    if ($answerModel->load($answer, '') && $answerModel->save()) {
+
+                    } elseif ($answerModel->hasErrors()) {
+                        $errors = $model->getFirstErrors();
+                        throw new DataValidationFailedException(array_pop($errors));
+                    } else {
+                        throw new ServerErrorHttpException();
+                    }
+                }
+            }
+
+            return Answer::find()
+                ->where(['id' => $model->id])
+                ->asArray()
+                ->with('answerList')
+                ->one();
         } elseif ($model->hasErrors()) {
             $errors = $model->getFirstErrors();
             throw new DataValidationFailedException(array_pop($errors));
@@ -104,133 +84,34 @@ class QuestionController extends Controller
         }
     }
 
-    /**
-     * 修改 类型
-     *
-     * POST 提交到 /activity-type/update?id=10
-     *
-     * ~~~
-     * {
-     *   "name": "户外1",
-     *   "displayorder": 96
-     * }
-     * ~~~
-     *
-     *
-     * @param $id
-     * @return array
-     *
-     * 成功
-     *
-     * ~~~
-     * {
-     *   "success": "1",
-     *   "data": {
-     *     "id": 10,
-     *     "name": "户外1",
-     *     "displayorder": 96,
-     *     "status": 10
-     *   },
-     *   "status_code": 200
-     * }
-     * ~~~
-     *
-     * 失败
-     *
-     * ~~~
-     * {
-     *   "success": "0",
-     *   "errmsg": "名称最少含有2个字符",
-     *   "status_code": 422
-     * }
-     * ~~~
-     *
-     * @throws DataValidationFailedException
-     * @throws NotFoundHttpException
-     * @throws ServerErrorHttpException
-     */
     public function actionUpdate($id)
     {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $model = $this->findModel($id);
-        $data = Yii::$app->getRequest()->post();
-
-        if (isset($data['name'])) {
-            $model->name = $data['name'];
-            if (!$model->validate('name')) {
-                throw new DataValidationFailedException($model->getFirstError('name'));
-            }
-        }
-
-        if (isset($data['displayorder'])) {
-            $model->displayorder = $data['displayorder'];
-            if (!$model->validate('displayorder')) {
-                throw new DataValidationFailedException($model->getFirstError('displayorder'));
-            }
-        }
-
-        if (isset($data['status'])) {
-            $model->status = $data['status'];
-            if (!$model->validate('status')) {
-                throw new DataValidationFailedException($model->getFirstError('status'));
-            }
-        }
-
-        if (!$model->save()) {
-            throw new ServerErrorHttpException();
-        }
-
-        return $this->findModel($id);
     }
 
-    /**
-     * 删除活动
-     * POST 请求 /activity-type/delete?id=10
-     *
-     * @param $id
-     * @return array
-     *
-     * 成功
-     *
-     * ~~~
-     * {
-     *   "success": "1",
-     *   "data": [],
-     *   "status_code": 200
-     * }
-     * ~~~
-     *
-     * @throws NotFoundHttpException
-     * @throws ServerErrorHttpException
-     * @throws \Exception
-     */
     public function actionDelete($id)
     {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $model = $this->findModel($id);
-        if ($model->delete() === false) {
-            throw new ServerErrorHttpException('删除失败');
-        }
-
-        return [];
     }
 
     public function actionView($id)
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
-        $model = $this->findModel($id);
+        $model = Question::find()
+            ->where(['id' => $id])
+            ->asArray()
+            ->with('questionList')
+            ->one();
 
         return $model;
     }
 
     /**
      * @param $id
-     * @return ActivityType
+     * @return Answer
      * @throws NotFoundHttpException
      */
     public function findModel($id)
     {
-        $model = ActivityType::findOne($id);
+        $model = Answer::findOne($id);
 
         if (isset($model)) {
             return $model;
