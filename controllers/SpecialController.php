c765af7104924b65223c90ca16ee7a2c120c492d
maxwelldu <dcj3sjt@163.com>
2015-10-09 17:54:22 +0800

    add special controller

--- controllers/SpecialController.php
+++ controllers/SpecialController.php
@@ -1,15 +1,277 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michaeldu
- * Date: 15/10/9
- * Time: 下午5:09
- */
 
 namespace app\controllers;
 
+use app\components\DataValidationFailedException;
+use app\models\Special;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+use yii\web\ServerErrorHttpException;
 
-class SpecialController
+class SpecialController extends Controller
 {
 
+    public $enableCsrfValidation = false;
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'index' => ['get'],
+                    'create' => ['post'],
+                    'update' => ['post'],
+                    'delete' => ['post'],
+                    'view' => ['get'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * 专题列表
+     *
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function actionIndex()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $types = Special::find()
+            ->orderBy([
+                'displayorder' => SORT_ASC,
+                'id' => SORT_ASC,
+            ])
+            ->all();
+
+        return $types;
+    }
+
+    /**
+     * 添加一个专题类型
+     *
+     * POST 请求 /special/create
+     *
+     * ~~~
+     * {
+     *   "title": <string: 专题标题>,
+     *   "desc": <string: 专题副标题>,
+     *   "poster": <string: 专题海报>,
+     *   "displayorder": <int: 排序，此字段为空为默认值 99>
+     * }
+     * ~~~
+     *
+     * @return  array
+     *
+     * 失败
+     *
+     * ~~~
+     * {
+     * "success": "0",
+     * "errmsg": "标题长度不得超过255个字符",
+     * "status_code": 422
+     * }
+     * ~~~
+     *
+     * 成功
+     *
+     * {
+     * "success": "1",
+     * "data": {
+     *   "id": 10,
+     *   "title": "国庆专题",
+     *   "displayorder": 99,
+     *   "status": 20
+     * },
+     * "status_code": 200
+     * }
+     *
+     * @throws DataValidationFailedException
+     * @throws ServerErrorHttpException
+     */
+    public function actionCreate()
+    {
+        $request = Yii::$app->getRequest();
+        $response = Yii::$app->getResponse();
+        $response->format = Response::FORMAT_JSON;
+
+        $data = $request->post();
+        $model = new Special;
+
+        if ($model->load($data, '') && $model->save()) {
+            return Special::findOne($model->id);
+        } elseif ($model->hasErrors()) {
+            $errors = $model->getFirstErrors();
+            throw new DataValidationFailedException(array_pop($errors));
+        } else {
+            throw new ServerErrorHttpException();
+        }
+    }
+
+    /**
+     * 修改 专题
+     *
+     * POST 提交到 /special/update?id=10
+     *
+     * ~~~
+     * {
+     *   "title": "国庆专题2",
+     *   "displayorder": 96
+     * }
+     * ~~~
+     *
+     *
+     * @param $id
+     * @return array
+     *
+     * 成功
+     *
+     * ~~~
+     * {
+     *   "success": "1",
+     *   "data": {
+     *     "id": 10,
+     *     "title": "国庆专题2",
+     *     "displayorder": 96,
+     *     "status": 20
+     *   },
+     *   "status_code": 200
+     * }
+     * ~~~
+     *
+     * 失败
+     *
+     * ~~~
+     * {
+     *   "success": "0",
+     *   "errmsg": "专题最少含有2个字符",
+     *   "status_code": 422
+     * }
+     * ~~~
+     *
+     * @throws DataValidationFailedException
+     * @throws NotFoundHttpException
+     * @throws ServerErrorHttpException
+     */
+    public function actionUpdate($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+        $data = Yii::$app->getRequest()->post();
+
+        if (isset($data['title'])) {
+            $model->title = $data['title'];
+            if (!$model->validate('title')) {
+                throw new DataValidationFailedException($model->getFirstError('title'));
+            }
+        }
+
+        if (isset($data['desc'])) {
+            $model->desc = $data['desc'];
+            if (!$model->validate('desc')) {
+                throw new DataValidationFailedException($model->getFirstError('desc'));
+            }
+        }
+
+        if (isset($data['poster'])) {
+            $model->poster = $data['poster'];
+            if (!$model->validate('poster')) {
+                throw new DataValidationFailedException($model->getFirstError('poster'));
+            }
+        }
+
+        if (isset($data['displayorder'])) {
+            $model->displayorder = $data['displayorder'];
+            if (!$model->validate('displayorder')) {
+                throw new DataValidationFailedException($model->getFirstError('displayorder'));
+            }
+        }
+
+        if (isset($data['status'])) {
+            $model->status = $data['status'];
+            if (!$model->validate('status')) {
+                throw new DataValidationFailedException($model->getFirstError('status'));
+            }
+        }
+
+        if (!$model->save()) {
+            throw new ServerErrorHttpException();
+        }
+
+        return $this->findModel($id);
+    }
+
+    /**
+     * 删除专题
+     * POST 请求 /special/delete?id=10
+     *
+     * @param $id
+     * @return array
+     *
+     * 成功
+     *
+     * ~~~
+     * {
+     *   "success": "1",
+     *   "data": [],
+     *   "status_code": 200
+     * }
+     * ~~~
+     *
+     * @throws NotFoundHttpException
+     * @throws ServerErrorHttpException
+     * @throws \Exception
+     */
+    public function actionDelete($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+        $model->status = Special::STATUS_DELETED;
+        if ($model->save() === false) {
+            throw new ServerErrorHttpException('删除失败');
+        }
+
+        return [];
+    }
+
+    public function actionView($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+
+        return $model;
+    }
+
+    /**
+     * @param $id
+     * @return ActivityType
+     * @throws NotFoundHttpException
+     */
+    public function findModel($id)
+    {
+        $model = Special::findOne($id);
+
+        if (isset($model)) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException("专题不存在");
+        }
+    }
 }
