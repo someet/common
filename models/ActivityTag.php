a50d4b128a7351f5ac7da88b6da094274a00c0bb
maxwelldu <dcj3sjt@163.com>
2015-10-14 16:27:22 +0800

    change model rule

--- models/ActivityTag.php
+++ models/ActivityTag.php
@@ -30,7 +30,15 @@ class ActivityTag extends \yii\db\ActiveRecord
             [['label'], 'required'],
             [['status'], 'integer'],
             [['label'], 'string', 'max' => 255],
-            [['label'], 'unique']
+            [['label'], 'unique', 'message' => '{attribute}已存在'],
+            [
+                'label',
+                'string',
+                'min' => 2,
+                'max' => 10,
+                'tooLong' => '{attribute}长度不得超过10个字符',
+                'tooShort' => '{attribute}最少含有2个字符',
+            ],
         ];
     }
 
