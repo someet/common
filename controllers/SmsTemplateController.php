d61504e0f8d8913531a6b2a911db04e0e2de3452
maxwelldu <dcj3sjt@163.com>
2015-10-15 15:47:48 +0800

    add sms template controller

--- controllers/SmsTemplateController.php
+++ controllers/SmsTemplateController.php
@@ -1,7 +1,261 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michaeldu
- * Date: 15/10/15
- * Time: 下午3:34
- */
+
+namespace app\controllers;
+
+use app\components\DataValidationFailedException;
+use app\models\SmsTemplate;
+use Yii;
+use yii\base\Exception;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+use yii\web\ServerErrorHttpException;
+
+class SmsTemplateController extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'index' => ['get'],
+                    'create' => ['post'],
+                    'update' => ['post'],
+                    'delete' => ['post'],
+                    'view' => ['get'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * 消息模板列表
+     *
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function actionIndex()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $types = SmsTemplate::find()
+            ->orderBy([
+                'id' => SORT_DESC,
+            ])
+            ->all();
+
+        return $types;
+    }
+
+    /**
+     * 添加一个消息模板
+     *
+     * POST 请求 /sms-template/create
+     *
+     * ~~~
+     * {
+     *   "name": <string: 消息名称>,
+     *   "template": <string: 模板>
+     * }
+     * ~~~
+     *
+     * @return  array
+     *
+     * 失败
+     *
+     * ~~~
+     * {
+     * "success": "0",
+     * "errmsg": "模板长度不得超过255个字符",
+     * "status_code": 422
+     * }
+     * ~~~
+     *
+     * 成功
+     *
+     * {
+     * "success": "1",
+     * "data": {
+     *   "id": 10,
+     *   "name": "审核通过",
+     *   "template": 99,
+     *   "status": 10
+     * },
+     * "status_code": 200
+     * }
+     *
+     * @throws DataValidationFailedException
+     * @throws ServerErrorHttpException
+     */
+    public function actionCreate()
+    {
+        $request = Yii::$app->getRequest();
+        $response = Yii::$app->getResponse();
+        $response->format = Response::FORMAT_JSON;
+
+        $data = $request->post();
+        $model = new SmsTemplate;
+
+        if ($model->load($data, '') && $model->save()) {
+            return SmsTemplate::findOne($model->id);
+        } elseif ($model->hasErrors()) {
+            $errors = $model->getFirstErrors();
+            throw new DataValidationFailedException(array_pop($errors));
+        } else {
+            throw new ServerErrorHttpException();
+        }
+    }
+
+    /**
+     * 修改 类型
+     *
+     * POST 提交到 /sms-template/update?id=10
+     *
+     * ~~~
+     * {
+     *   "name": "审核通过",
+     *   "template": "[审核通过], 恭喜您通过了",
+     * }
+     * ~~~
+     *
+     *
+     * @param $id
+     * @return array
+     *
+     * 成功
+     *
+     * ~~~
+     * {
+     *   "success": "1",
+     *   "data": {
+     *     "id": 10,
+     *     "name": "审核通过",
+     *     "template": "[Someet活动平台]恭喜您审核通过了",
+     *     "status": 10
+     *   },
+     *   "status_code": 200
+     * }
+     * ~~~
+     *
+     * 失败
+     *
+     * ~~~
+     * {
+     *   "success": "0",
+     *   "errmsg": "名称最少含有2个字符",
+     *   "status_code": 422
+     * }
+     * ~~~
+     *
+     * @throws DataValidationFailedException
+     * @throws NotFoundHttpException
+     * @throws ServerErrorHttpException
+     */
+    public function actionUpdate($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+        $data = Yii::$app->getRequest()->post();
+
+        if (isset($data['name'])) {
+            $model->name = $data['name'];
+            if (!$model->validate('name')) {
+                throw new DataValidationFailedException($model->getFirstError('name'));
+            }
+        }
+
+        if (isset($data['template'])) {
+            $model->template = $data['template'];
+            if (!$model->validate('template')) {
+                throw new DataValidationFailedException($model->getFirstError('template'));
+            }
+        }
+
+        if (isset($data['status'])) {
+            $model->status = $data['status'];
+            if (!$model->validate('status')) {
+                throw new DataValidationFailedException($model->getFirstError('status'));
+            }
+        }
+
+        if (!$model->save()) {
+            throw new ServerErrorHttpException();
+        }
+
+        return $this->findModel($id);
+    }
+
+    /**
+     * 删除消息模板
+     * POST 请求 /sms-template/delete?id=10
+     *
+     * @param $id
+     * @return array
+     *
+     * 成功
+     *
+     * ~~~
+     * {
+     *   "success": "1",
+     *   "data": [],
+     *   "status_code": 200
+     * }
+     * ~~~
+     *
+     * @throws NotFoundHttpException
+     * @throws ServerErrorHttpException
+     * @throws \Exception
+     */
+    public function actionDelete($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+
+        $model->status = SmsTemplate::STATUS_DELETED;
+        if ($model->save() === false) {
+            throw new ServerErrorHttpException('删除失败');
+        }
+
+        return [];
+    }
+
+    public function actionView($id)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $model = $this->findModel($id);
+
+        return $model;
+    }
+
+    /**
+     * @param $id
+     * @return SmsTemplate
+     * @throws NotFoundHttpException
+     */
+    public function findModel($id)
+    {
+        $model = SmsTemplate::findOne($id);
+
+        if (isset($model)) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException("消息模板不存在");
+        }
+    }
+}
