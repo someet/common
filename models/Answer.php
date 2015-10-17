9d96c6e8d37469373240e153d04d6eff45d2cf16
maxwelldu <dcj3sjt@163.com>
2015-10-17 15:38:34 +0800

    Squashed 'packages/someet-common/' changes from 58dde7f..2a650a8
    2a650a8 delete app\models
    20c3908 move migrations to common repo
    
    git-subtree-dir: packages/someet-common
    git-subtree-split: 2a650a806fe7e2cf8d620df3394f06aeaace2a1f

---
+++ models/Answer.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace someet\common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "answer".
+ *
+ * @property integer $id
+ * @property integer $question_id
+ * @property integer $activity_id
+ * @property integer $user_id
+ * @property integer $is_finish
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $status
+ */
+class Answer extends \yii\db\ActiveRecord
+{
+    /* 未审核 */
+    const STATUS_REVIEW_YET     = 10;
+    /* 审核通过 */
+    const STATUS_REVIEW_PASS    = 20;
+    /* 审核拒绝 */
+    const STATUS_REVIEW_REJECT  = 30;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'answer';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['question_id'], 'required'],
+            [['question_id', 'activity_id', 'user_id', 'is_finish', 'created_at', 'updated_at', 'status'], 'integer'],
+            [['question_id', 'user_id'], 'unique', 'targetAttribute' => ['question_id', 'user_id'], 'message' => 'The combination of 问题ID and 用户ID has already been taken.']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'question_id' => '问题ID',
+            'activity_id' => '活动ID',
+            'user_id' => '用户ID',
+            'is_finish' => '0 进行中 1 已完成',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'status' => '0 删除 10 正常',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => behaviors\TimestampBehavior::className(),
+            ],
+        ];
+    }
+
+    public function getAnswerItemList()
+    {
+        return $this->hasMany(AnswerItem::className(), ['question_id' => 'question_id', 'user_id' => 'user_id']);
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
