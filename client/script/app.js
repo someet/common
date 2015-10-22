4775a3222fba0bd698664ba222b1e3a7e542cd50
maxwelldu <dcj3sjt@163.com>
2015-10-22 15:47:01 +0800

    add time transfer function

--- client/script/app.js
+++ client/script/app.js
@@ -18,3 +18,12 @@ angular.module('backendServices', []);
 app.config(["$httpProvider", function($httpProvider) {
   $httpProvider.defaults.headers.post['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 }]);
+
+// 根据时间的字符串获得时间戳
+function getTimestamp(str) {
+  return (new Date(str)).getTime() / 1000;
+}
+// 根据时间戳获取时间的字符串
+function getTimeByTimestamp(timestamp) {
+  return new Date(parseInt(timestamp) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
+}
