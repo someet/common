07fbd77ae14b1ebf06f820b8710ccfc9b538c3c9
maxwelldu <dcj3sjt@163.com>
2015-10-23 16:14:04 +0800

    set alias

--- web/index.php
+++ web/index.php
@@ -3,4 +3,5 @@ require('/var/www/html/helpers/DockerEnv.php');
 \DockerEnv::init();
 $config = \DockerEnv::webConfig();
 \Yii::setAlias('common', dirname(__DIR__) . '/packages/someet-common/');
+\Yii::setAlias('dektrium-user', dirname(__DIR__) . '/packages/yii2-user/');
 (new yii\web\Application($config))->run();
