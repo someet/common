4af1e976c1d930de0f56342f9a0fac062cb93bb4
iLukeYU <ilukeyu@icloud.com>
2015-10-26 22:07:32 +0800

    add activity page

--- client/script/controllers/ActivityCtrl.js
+++ client/script/controllers/ActivityCtrl.js
@@ -1,255 +1,304 @@
 angular.module('controllers', ['ngTagsInput'])
-    .controller('ActivityCtrl',
-    ['$scope', '$location', '$activityManage', '$mdDialog', 'lodash', '$mdToast',
-        function ($scope, $location, $activityManage, $mdDialog, lodash, $mdToast) {
+  .controller('ActivityCtrl', ['$scope', '$location', '$activityManage', '$activityTypeManage', '$mdDialog', 'lodash', '$mdToast',
+    function($scope, $location, $activityManage, $activityTypeManage, $mdDialog, lodash, $mdToast) {
 
-            $scope.$parent.pageName = '活动管理';
-            $activityManage.fetch().then(function (data) {
-                $scope.list = data;
-            }, function (err) {
-                alert(err);
+      $scope.$parent.pageName = '活动管理';
+      $activityManage.fetch().then(function(data) {
+        $scope.list = data;
+      }, function(err) {
+        alert(err);
+      });
+
+      // 活动类型列表
+      $activityTypeManage.fetch().then(function(data) {
+        $scope.activityTypeList = data;
+      }, function(err) {
+        alert(err);
+      });
+
+      $scope.onTypeAddClicked = function() {
+        $scope.showAddForm = true;
+      };
+
+      //ng-if会增加新的child，需要设置初始值
+      $scope.addForm = {
+        newType: ""
+      };
+
+      $scope.commitTypeName = function(data) {
+        var newEntity = {
+          name: data,
+          display_order: 3
+        };
+        $activityTypeManage.create(newEntity).then(function(data) {
+          $activityTypeManage.fetch().then(function(data) {
+            $scope.activityTypeList = data;
+          }, function(err) {
+            alert(err);
+          });
+
+          $location.path('/activity');
+          $mdToast.show($mdToast.simple()
+            .content('添加活动类型成功')
+            .hideDelay(5000)
+            .position("top right"));
+        }, function(err) {
+          $mdToast.show($mdToast.simple()
+            .content(err.toString())
+            .hideDelay(5000)
+            .position("top right"));
+        });
+        $scope.showAddForm = false;
+        $scope.addForm = {
+          newType: ""
+        };
+      };
+
+      // 置顶/取消置顶
+      $scope.top = function(entity, is_top) {
+        var newEntity = entity;
+        newEntity.is_top = is_top > 0 ? 1 : 0; // 是否置顶
+        $activityManage.update(entity.id, newEntity).then(function(data) {
+          $mdToast.show($mdToast.simple()
+            .content('置顶成功')
+            .hideDelay(5000)
+            .position("top right"));
+          $location.path('/activity');
+        }, function(err) {
+          $mdToast.show($mdToast.simple()
+            .content(err.toString())
+            .hideDelay(5000)
+            .position("top right"));
+        })
+      }
+
+      // 预览问题
+      $scope.previewQuestion = function(entity) {
+        $location.path('/answer/view/' + entity.id);
+      }
+
+      $scope.update = function(entity) {
+        $location.path('/activity/' + entity.id);
+      };
+
+      $scope.delete = function(entity) {
+
+        var confirm = $mdDialog.confirm()
+          .title('确定要删除活动“' + entity.title + '”吗？')
+          .ariaLabel('delete activity item')
+          .ok('确定删除')
+          .cancel('手滑点错了，不删');
+
+        $mdDialog.show(confirm).then(function() {
+          $activityManage.delete(entity).then(function(data) {
+
+            lodash.remove($scope.list, function(tmpRow) {
+              return tmpRow == entity;
             });
 
-          // 置顶/取消置顶
-          $scope.top = function(entity, is_top) {
-            var newEntity = entity;
-            newEntity.is_top = is_top > 0 ? 1 : 0; // 是否置顶
-            $activityManage.update(entity.id, newEntity).then(function(data){
-              $mdToast.show($mdToast.simple()
-                  .content('置顶成功')
-                  .hideDelay(5000)
-                  .position("top right"));
-              $location.path('/activity');
-            }, function(err) {
-              $mdToast.show($mdToast.simple()
-                  .content(err.toString())
-                  .hideDelay(5000)
-                  .position("top right"));
-            })
-          }
+            $mdToast.show($mdToast.simple()
+              .content('删除活动类型“' + entity.title + '”成功')
+              .hideDelay(5000)
+              .position("top right"));
 
-          // 预览问题
-          $scope.previewQuestion = function(entity) {
-            $location.path('/answer/view/'+entity.id);
-          }
+          }, function(err) {
+            $mdToast.show($mdToast.simple()
+              .content(err.toString())
+              .hideDelay(5000)
+              .position("top right"));
+          });
+        });
+      };
+      $scope.createPage = function() {
+        $location.path('/activity/add');
+      }
 
-            $scope.update = function (entity) {
-                $location.path('/activity/' + entity.id);
-            };
+      // 设置问题
+      $scope.viewQuestion = function(activity) {
+        $location.path('/question/view/' + activity.id);
+      }
 
-            $scope.delete = function (entity) {
+      $scope.viewAnswer = function(activity) {
+        $location.path('/answer/' + activity.id);
+      }
 
-                var confirm = $mdDialog.confirm()
-                    .title('确定要删除活动“' + entity.title + '”吗？')
-                    .ariaLabel('delete activity item')
-                    .ok('确定删除')
-                    .cancel('手滑点错了，不删');
+    }
+  ])
+  .controller('ActivityViewCtrl', ['$scope', '$routeParams', '$location', '$activityManage', '$activityTypeManage', '$qupload', '$qiniuManage', '$mdToast',
+    function($scope, $routeParams, $location, $activityManage, $activityTypeManage, $qupload, $qiniuManage, $mdToast) {
+      $scope.$parent.pageName = '活动详情';
 
-                $mdDialog.show(confirm).then(function () {
-                    $activityManage.delete(entity).then(function (data) {
+      $scope.onStartTimeSet = function(newDate, oldDate) {
+        $scope.entity.start_time = getTimeByTimestamp(getTimestamp(newDate));
+      }
 
-                        lodash.remove($scope.list, function (tmpRow) {
-                            return tmpRow == entity;
-                        });
+      $scope.onStopTimeSet = function(newDate, oldDate) {
+        $scope.entity.end_time = getTimeByTimestamp(getTimestamp(newDate));
+      }
 
-                        $mdToast.show($mdToast.simple()
-                            .content('删除活动类型“' + entity.title + '”成功')
-                            .hideDelay(5000)
-                            .position("top right"));
+      // 标签
+      $scope.tags = [];
+      // 标签搜索功能
+      $scope.loadTags = function(query) {
+        return $activityManage.tags(query);
+      };
 
-                    }, function (err) {
-                        $mdToast.show($mdToast.simple()
-                            .content(err.toString())
-                            .hideDelay(5000)
-                            .position("top right"));
-                    });
-                });
-            };
-            $scope.createPage = function() {
-                $location.path('/activity/add');
+      // qiniu upload poster start //
+      $scope.selectPoster = null;
+
+      var startPoster = function() {
+        $qiniuManage.fetchUploadToken().then(function(token) {
+
+          $qupload.upload({
+            key: '',
+            file: $scope.selectPoster.file,
+            token: token
+          }).then(function(response) {
+            $qiniuManage.completelyUrl(response.key).then(function(url) {
+              $scope.poster = url;
+            });
+          }, function(response) {
+            console.log(response);
+          }, function(evt) {
+            if ($scope.selectPoster !== null) {
+              $scope.selectPoster.progress.p = Math.floor(100 * evt.loaded / evt.totalSize);
             }
+          });
 
-          // 设置问题
-          $scope.viewQuestion = function(activity) {
-            $location.path('/question/view/' + activity.id);
+        });
+      };
+
+      $scope.posterAbort = function() {
+        $scope.selectPoster.upload.abort();
+        $scope.selectPoster = null;
+      };
+
+      $scope.onPosterSelect = function($files) {
+        $scope.selectPoster = {
+          file: $files[0],
+          progress: {
+            p: 0
           }
+        };
+        startPoster();
+      };
+      // qiniu upload poster end //
 
-          $scope.viewAnswer = function(activity) {
-            $location.path('/answer/' + activity.id);
-          }
+      // qiniu upload code start //
+      $scope.selectCode = null;
 
-        }])
-    .controller('ActivityViewCtrl',
-    ['$scope', '$routeParams', '$location', '$activityManage', '$activityTypeManage', '$qupload', '$qiniuManage', '$mdToast',
-        function ($scope, $routeParams, $location, $activityManage, $activityTypeManage, $qupload, $qiniuManage, $mdToast) {
-            $scope.$parent.pageName = '活动详情';
+      var startCode = function() {
+        $qiniuManage.fetchUploadToken().then(function(token) {
 
-          $scope.onStartTimeSet = function (newDate, oldDate) {
-            $scope.entity.start_time = getTimeByTimestamp(getTimestamp(newDate));
-          }
-
-          $scope.onStopTimeSet = function (newDate, oldDate) {
-            $scope.entity.end_time = getTimeByTimestamp(getTimestamp(newDate));
-          }
-
-          // 标签
-          $scope.tags = [];
-          // 标签搜索功能
-          $scope.loadTags = function(query) {
-            return $activityManage.tags(query);
-          };
-
-          // qiniu upload poster start //
-          $scope.selectPoster = null ;
-
-          var startPoster = function () {
-            $qiniuManage.fetchUploadToken().then(function (token) {
-
-              $qupload.upload({
-                key: '',
-                file: $scope.selectPoster.file,
-                token: token
-              }).then(function (response) {
-                $qiniuManage.completelyUrl(response.key).then(function(url) {
-                  $scope.poster = url;
-                });
-              }, function (response) {
-                console.log(response);
-              }, function (evt) {
-                if($scope.selectPoster !== null){
-                  $scope.selectPoster.progress.p = Math.floor(100 * evt.loaded / evt.totalSize);
-                }
-              });
-
+          $qupload.upload({
+            key: '',
+            file: $scope.selectCode.file,
+            token: token
+          }).then(function(response) {
+            $qiniuManage.completelyUrl(response.key).then(function(url) {
+              $scope.group_code = url;
             });
-          };
-
-          $scope.posterAbort = function () {
-            $scope.selectPoster.upload.abort();
-            $scope.selectPoster = null;
-          };
-
-          $scope.onPosterSelect = function ($files) {
-            $scope.selectPoster = {
-              file: $files[0],
-              progress: {p: 0}
-            };
-            startPoster();
-          };
-          // qiniu upload poster end //
-
-          // qiniu upload code start //
-          $scope.selectCode = null ;
-
-          var startCode = function () {
-            $qiniuManage.fetchUploadToken().then(function (token) {
-
-              $qupload.upload({
-                key: '',
-                file: $scope.selectCode.file,
-                token: token
-              }).then(function (response) {
-                $qiniuManage.completelyUrl(response.key).then(function(url) {
-                  $scope.group_code = url;
-                });
-              }, function (response) {
-                console.log(response);
-              }, function (evt) {
-                if($scope.selectCode !== null){
-                  $scope.selectCode.progress.p = Math.floor(100 * evt.loaded / evt.totalSize);
-                }
-              });
-
-            });
-          };
-
-          $scope.codeAbort = function () {
-            $scope.selectCode.upload.abort();
-            $scope.selectCode = null;
-          };
-
-          $scope.onCodeSelect = function ($files) {
-            $scope.selectCode = {
-              file: $files[0],
-              progress: {p: 0}
-            };
-            startCode();
-          };
-          // qiniu upload group code end //
-
-            var id = $routeParams.id;
-            if(id>0) {
-              $activityManage.fetch(id).then(function (data) {
-                $scope.entity = data;
-                $scope.entity.start_time = getTimeByTimestamp(data.start_time);
-                $scope.entity.end_time = getTimeByTimestamp(data.end_time);
-                $scope.poster = data.poster;
-                $scope.group_code = data.group_code;
-
-                var tags = [];
-                for(var k in data.tags) {
-                  var tag = data.tags[k].name;
-                  tags.push(tag);
-                }
-                $scope.tags = tags;
-
-              }, function (err) {
-                  $location.path('/activity');
-              });
+          }, function(response) {
+            console.log(response);
+          }, function(evt) {
+            if ($scope.selectCode !== null) {
+              $scope.selectCode.progress.p = Math.floor(100 * evt.loaded / evt.totalSize);
             }
+          });
 
-          // 列表
-            $activityTypeManage.fetch().then(function(data){
-                $scope.type_list = data;
-            });
+        });
+      };
 
-          // 取消
-            $scope.cancel = function() {
-                $location.path('/activity/');
-            }
+      $scope.codeAbort = function() {
+        $scope.selectCode.upload.abort();
+        $scope.selectCode = null;
+      };
+
+      $scope.onCodeSelect = function($files) {
+        $scope.selectCode = {
+          file: $files[0],
+          progress: {
+            p: 0
+          }
+        };
+        startCode();
+      };
+      // qiniu upload group code end //
+
+      var id = $routeParams.id;
+      if (id > 0) {
+        $activityManage.fetch(id).then(function(data) {
+          $scope.entity = data;
+          $scope.entity.start_time = getTimeByTimestamp(data.start_time);
+          $scope.entity.end_time = getTimeByTimestamp(data.end_time);
+          $scope.poster = data.poster;
+          $scope.group_code = data.group_code;
+
+          var tags = [];
+          for (var k in data.tags) {
+            var tag = data.tags[k].name;
+            tags.push(tag);
+          }
+          $scope.tags = tags;
+
+        }, function(err) {
+          $location.path('/activity');
+        });
+      }
+
+      // 列表
+      $activityTypeManage.fetch().then(function(data) {
+        $scope.type_list = data;
+      });
+
+      // 取消
+      $scope.cancel = function() {
+        $location.path('/activity/');
+      }
 
 
-            $scope.save = function () {
-                var newEntity = $scope.entity;
-                newEntity.start_time = getTimestamp($scope.entity.start_time);
-                newEntity.end_time = getTimestamp($scope.entity.end_time);
-                newEntity.poster = $scope.poster;
-                newEntity.group_code = $scope.group_code;
+      $scope.save = function() {
+        var newEntity = $scope.entity;
+        newEntity.start_time = getTimestamp($scope.entity.start_time);
+        newEntity.end_time = getTimestamp($scope.entity.end_time);
+        newEntity.poster = $scope.poster;
+        newEntity.group_code = $scope.group_code;
 
-                var tags = [];
-                for(var k in $scope.tags) {
-                  var tag = $scope.tags[k].text;
-                  tags.push(tag);
-                }
-                newEntity.tagNames = tags.join();
+        var tags = [];
+        for (var k in $scope.tags) {
+          var tag = $scope.tags[k].text;
+          tags.push(tag);
+        }
+        newEntity.tagNames = tags.join();
 
-                if (newEntity.id > 0 ) { // 更新活动
-                    $activityManage.update(newEntity.id, newEntity).then(function(data) {
-                        $mdToast.show($mdToast.simple()
-                            .content('活动保存成功')
-                            .hideDelay(5000)
-                            .position("top right"));
-                        $location.path('/activity');
-                    }, function(err){
-                        $mdToast.show($mdToast.simple()
-                            .content(err.toString())
-                            .hideDelay(5000)
-                            .position("top right"));
-                    });
-                } else { // 添加活动
-                    $activityManage.create(newEntity).then(function (data) {
-                      $location.path('/activity');
-                        $mdToast.show($mdToast.simple()
-                            .content('活动添加成功')
-                            .hideDelay(5000)
-                            .position("top right"));
-                    }, function (err) {
-                        $mdToast.show($mdToast.simple()
-                            .content(err.toString())
-                            .hideDelay(5000)
-                            .position("top right"));
-                    });
-                }
-            };
+        if (newEntity.id > 0) { // 更新活动
+          $activityManage.update(newEntity.id, newEntity).then(function(data) {
+            $mdToast.show($mdToast.simple()
+              .content('活动保存成功')
+              .hideDelay(5000)
+              .position("top right"));
+            $location.path('/activity');
+          }, function(err) {
+            $mdToast.show($mdToast.simple()
+              .content(err.toString())
+              .hideDelay(5000)
+              .position("top right"));
+          });
+        } else { // 添加活动
+          $activityManage.create(newEntity).then(function(data) {
+            $location.path('/activity');
+            $mdToast.show($mdToast.simple()
+              .content('活动添加成功')
+              .hideDelay(5000)
+              .position("top right"));
+          }, function(err) {
+            $mdToast.show($mdToast.simple()
+              .content(err.toString())
+              .hideDelay(5000)
+              .position("top right"));
+          });
+        }
+      };
 
-        }]);
+    }
+  ]);
