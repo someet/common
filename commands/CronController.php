6e07b81451e7eb618b5e2b65b899a702d0cebe49
maxwelldu <dcj3sjt@163.com>
2015-11-05 20:09:06 +0800

    update send sms crond

--- commands/CronController.php
+++ commands/CronController.php
@@ -40,10 +40,32 @@ class CronController  extends \yii\console\Controller
         }
     }
 
+    /**
+     * 发送短信通知审核未通过
+     */
     public function actionSendNotReviewSms()
     {
         // 给审核不通过的用户发短信
         $answerList = Answer::find()->where(['status' => Answer::STATUS_REVIEW_REJECT])->with(['user', 'activity'])->all();
+        foreach($answerList as $answer) {
+            $mobile = $answer->user->mobile;
+            $activityName = $answer->activity->title;
+            if ($this->isTelNumber($mobile)) {
+                $r = Yii::$app->yunpian->sendSms($mobile, "【Someet活动平台】Someet用户您好，很抱歉您报名的“{$activityName}”活动未通过筛选。关于如何提高报名的成功率，这里有几个小tips，1.认真回答筛选问题； 2.尽早报名，每周二周三是活动推送时间，周四周五报名的成功概率会相对降低很多 3.自己发起活动，优质的发起人是有参与活动特权的哦~ 当然，您还可以添加我们的官方客服Someet小海豹（微信号：someetxhb）随时与我们联系。期待下次活动和你相遇。系统短信，请勿回复。");
+                echo !$r ? '手机号: '.$mobile.', 审核通过的短信发送失败' : '手机号:'.$mobile.', 审核通过的短信发送成功';
+                echo "\r\n";
+            }
+            Answer::updateAll(['is_send' => '1', 'send_at' => time()], ['id' => $answer->id]);
+        }
+    }
+
+    /**
+     * 发送短信通知审核未通过
+     */
+    public function actionSendWaitSms()
+    {
+        // 给审核不通过的用户发短信
+        $answerList = Answer::find()->where(['status' => Answer::STATUS_REVIEW_YET])->with(['user', 'activity'])->all();
         foreach($answerList as $answer) {
             $mobile = $answer->user->mobile;
             $activityName = $answer->activity->title;
