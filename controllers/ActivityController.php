bb4eb531e24ea8fdfff7ac4bde6e46b7a9b25a07
maxwelldu <dcj3sjt@163.com>
2015-11-13 20:47:47 +0800

    add preview link

--- controllers/ActivityController.php
+++ controllers/ActivityController.php
@@ -96,6 +96,7 @@ class ActivityController extends BackendController
         }
         foreach($activities as $key => $activity) {
             $activities[$key]['feedback_count'] = count($activity['feedbackList']);
+            $activities[$key]['preview_url'] = Yii::$app->params['domain'].'preview/'.$activity['id'];
         }
 
 
