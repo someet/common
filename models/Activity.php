71d68a7db77cf7901359e05cbded5c5e147d5c97
maxwelldu <dcj3sjt@163.com>
2015-11-19 15:19:23 +0800

    use local css and js

--- models/Activity.php
+++ models/Activity.php
@@ -129,7 +129,11 @@ class Activity extends \yii\db\ActiveRecord
     {
         if (parent::beforeSave($insert)) {
             if ($insert) {
-                $this->updated_by = $this->created_by = Yii::$app->user && Yii::$app->user->id > 0 ? Yii::$app->user->id : 0;
+                if ($this->created_by < 1) {
+                    $this->updated_by = $this->created_by = Yii::$app->user && Yii::$app->user->id > 0 ? Yii::$app->user->id : 0;
+                } else {
+                    $this->updated_by = $this->created_by;
+                }
             } else {
                 $this->updated_by = Yii::$app->user && Yii::$app->user->id > 0 ? Yii::$app->user->id : 0;
             }
