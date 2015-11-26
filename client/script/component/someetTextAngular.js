35fb4f4a633ca3f2e6399d4065a005087c3a94db
langshuang <409909041@qq.com>
2015-11-26 20:59:10 +0800

    修正 someetTextAngular

--- client/script/component/someetTextAngular.js
+++ client/script/component/someetTextAngular.js
@@ -1,42 +1,38 @@
 // custom text angular image insert image link and video insert
 angular.module('someetTextAngular', ['textAngular'])
   .config(['$provide', function($provide) {
-    // taOptions 就卡在这了
-    //$provide.decorator('taOptions',function($delegate){
 
-    //});
+    $provide.decorator('taOptions', ['$delegate', 'taRegisterTool', '$uploadService', '$http', '$window', '$mdToast',
+      function(taOptions, taRegisterTool, $uploadService, $http, $window, $mdToast) {
+        var imgSize = 300000,
+          imgSizeKb = (imgSize / 1000) + 'KB';
 
-    //$provide.decorator('taOptions', ['$delegate', 'taRegisterTool', '$uploadService', '$http', '$window', '$mdToast',
-    //  function(taOptions, taRegisterTool, $uploadService, $http, $window, $mdToast) {
-    //    var imgSize = 300000,
-    //      imgSizeKb = (imgSize / 1000) + 'KB';
-    //
-    //    //taOptions.toolbar = [
-    //    //  ['redo', 'undo', 'justifyLeft','justifyCenter','justifyRight','html', 'tudouVideo']
-    //    //];
-    //
-    //    taOptions.defaultFileDropHandler = function(file, insertAction) {
-    //      if (file.size >= imgSize) {
-    //        $mdToast.show($mdToast.simple({
-    //          position: 'bottom right'
-    //        }).content('图片的大小超过' + imgSizeKb + '限制'));
-    //
-    //        return;
-    //      }
-    //
-    //      $uploadService.upload(file).then(function(filename) {
-    //        var imgsrc = 'http://7te94f.com2.z0.glb.qiniucdn.com/' + filename;
-    //
-    //        insertAction('insertImage', imgsrc, true);
-    //      });
-    //
-    //      return true;
-    //    };
-    //
-    //
-    //    return taOptions;
-    //  }]);
-    //
+        //taOptions.toolbar = [
+        //  ['redo', 'undo', 'justifyLeft','justifyCenter','justifyRight','html', 'tudouVideo']
+        //];
+
+        taOptions.defaultFileDropHandler = function(file, insertAction) {
+          if (file.size >= imgSize) {
+            $mdToast.show($mdToast.simple({
+              position: 'bottom right'
+            }).content('图片的大小超过' + imgSizeKb + '限制'));
+
+            return;
+          }
+
+          $uploadService.upload(file).then(function(filename) {
+            var imgsrc = 'http://7te94f.com2.z0.glb.qiniucdn.com/' + filename;
+
+            insertAction('insertImage', imgsrc, true);
+          });
+
+          return true;
+        };
+
+
+        return taOptions;
+      }]);
+
 
   }]);
 
