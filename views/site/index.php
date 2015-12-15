f78e7c3a094104456239ea9422446db16dcbc0c4
iLukeYU <ilukeyu@icloud.com>
2015-12-15 11:06:39 +0800

    add lost pictures and fix css bug

--- views/site/index.php
+++ views/site/index.php
@@ -21,7 +21,7 @@ use yii\helpers\Url;
   <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
   <!--script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script-->
   <link rel="stylesheet" href="/static/style/main.css">
-  <link rel="stylesheet" href="/css/mystyle.css">
+  <link rel="stylesheet" href="/static/style/mystyle.css">
 
   <?= Html::csrfMetaTags() ?></head>
 <body class="main_css layout-column flex"  flex="" layout="column" ng-controller="MainCtrl">
