25524a6e22033e6fd218d3a2adcc7af7ad3548a3
maxwelldu <dcj3sjt@163.com>
2016-02-17 19:51:07 +0800

    Squashed 'packages/someet-common/' changes from b42dfae..b0d865c
    b0d865c Merge commit 'd38c1908606be2703bd1b38f6c048155b977a1bb' into add_access
    88091c6 add access
    09128bd add filter permission
    5e00c8a add permission
    34264df add permission
    1c176f6 add permission
    78360bd add permission
    ae8d0d9 add role
    bd4d20c add role
    
    git-subtree-dir: packages/someet-common
    git-subtree-split: b0d865ccca5bf208005011b984ff874feaad558b

---
+++ migrations/m160210_075520_add_founder_permission.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160210_075520_add_founder_permission extends Migration
+{
+    public function up()
+    {
+        $items = [
+            '/mobile/member/preview-feedback' => ['founder'],
+        ];
+
+        $authItemTemplate = <<<SQL
+INSERT INTO auth_item (name, type, description, rule_name, data, created_at, updated_at) VALUES ('%s', '2', '', null, null, null, null);
+SQL;
+        $itemChildTemplate = <<<SQL
+        INSERT INTO auth_item_child (parent, child) VALUES ('%s', '%s');
+SQL;
+        $sql = '';
+        foreach ($items as $item => $roles) {
+            $sql .= sprintf($authItemTemplate, $item);
+            foreach ($roles as $role) {
+                $sql .= sprintf($itemChildTemplate, $role, $item);
+            }
+        }
+        $this->execute($sql);
+        return true;
+    }
+
+    public function down()
+    {
+        echo "m160210_075520_add_founder_permission cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
