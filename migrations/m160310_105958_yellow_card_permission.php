c6ad5c7365c85f129e7ae5f14fe3d0e1b41aedfc
wshudong <wsd312@163.com>
2016-03-11 10:41:46 +0800

    yellow card backend

---
+++ migrations/m160310_105958_yellow_card_permission.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160310_105958_yellow_card_permission extends Migration
+{
+    public function up()
+    {
+        $items = [
+            /* 答案审核 */
+            '/backend/member/yellow-card' => ['pma'],
+            '/backend/member/abandon-yellow-card' => ['pma'],
+        ];
+
+        $authItemTemplate = <<<SQL
+INSERT INTO auth_item (name, type, description, rule_name, data, created_at, updated_at) VALUES ('%s', '2', '', null, null, null, null);
+SQL;
+        $itemChildTemplate = <<<SQL
+        INSERT INTO auth_item_child (parent, child) VALUES ('%s', '%s');
+SQL;
+        $sql = '';
+        foreach ($items as $item => $roles) {
+            $sql .= sprintf($authItemTemplate, $item);
+            foreach ($roles as $role) {
+                $sql .= sprintf($itemChildTemplate, $role, $item);
+            }
+        }
+        $this->execute($sql);
+        return true;
+    }
+
+    public function down()
+    {
+        echo "m160310_105958_yellow_card_permission cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
