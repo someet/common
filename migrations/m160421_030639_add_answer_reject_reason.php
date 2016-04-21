160c54fc157448adf86add62104e2aff99b830cc
wshudong <wsd312@163.com>
2016-04-21 11:11:53 +0800

    reject migrate

---
+++ migrations/m160421_030639_add_answer_reject_reason.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160421_030639_add_answer_reject_reason extends Migration
+{
+    public function up()
+    {
+        $sql = <<<SQL
+ALTER TABLE `answer` ADD COLUMN `reject_reason` varchar(255) NOT NULL DEFAULT 0 COMMENT '拒绝原因';
+SQL;
+        $this->execute($sql);
+        return true;
+    }
+
+    public function down()
+    {
+        $this->dropTable('activity_check_in');
+        return true;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
