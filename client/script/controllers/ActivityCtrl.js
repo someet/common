a004372d15b9aed761845076ea352945ebb76033
wshudong <wsd312@163.com>
2016-05-12 16:06:25 +0800

    models r activity sapce

--- client/script/controllers/ActivityCtrl.js
+++ client/script/controllers/ActivityCtrl.js
@@ -486,17 +486,24 @@ angular.module('controllers', ['ngTagsInput'])
                 // $scope.sections = $scope.space_spot.sections;
                 // console.log($scope.sections);
                 return $scope.space_spot;
-                // $activityManage.searchSpace(query).then(function(data) {
-                //     // $scope.section = data.sections;
-                //     // $scope.space_spot = data;
-                //     // console.log(data.models.sections);
-                //     // console.log(data.sections);
-                //     // console.log(data);
-                //     return data;
-                // });
-
             }
 
+            
+      $scope.selectedToppings = [];
+      $scope.printSelectedToppings = function printSelectedToppings() {
+        var numberOfToppings = this.selectedToppings.length;
+        // If there is more than one topping, we add an 'and'
+        // to be gramatically correct. If there are 3+ toppings
+        // we also add an oxford comma.
+        if (numberOfToppings > 1) {
+          var needsOxfordComma = numberOfToppings > 2;
+          var lastToppingConjunction = (needsOxfordComma ? ',' : '') + ' and ';
+          var lastTopping = lastToppingConjunction +
+              this.selectedToppings[this.selectedToppings.length - 1];
+          return this.selectedToppings.slice(0, -1).join(', ') + lastTopping;
+        }
+        return this.selectedToppings.join('');
+      };
 
             // 获取场地
             $activityManage.searchSpace('').then(function(data) {
@@ -510,21 +517,14 @@ angular.module('controllers', ['ngTagsInput'])
 
             //搜索空间
             $scope.getSection = function(obj) {
-                // console.log($scope.selectedSpaceSpot);
-                console.log(obj);
+
+                if (obj != null) {
+                // 把字符转化为对象
+                var obj = JSON.parse(obj)
+                console.log(typeof obj);
                 $scope.sections = obj.sections;
 
-                console.log($scope.sections);
-                // $scope.sections = '';
-                // return $activityManage.fetch(id);
-                // if (space_spot != null) {
-                //     $scope.sections = space_spot.sections;
-                //     console.log(space_spot.sections);
-                //     return $scope.sections;
-                // } else {
-                //     $scope.sections = {};
-                //     console.log(22222);
-                // }
+                }
             }  
 
             // 搜索用户功能
