895cd2d096fecc7e823c5511f5a75d8a6ad11405
wshudong <wsd312@163.com>
2016-05-16 16:59:49 +0800

    models r activity sapce

--- migrations/m160516_084717_add_space_spot_id.php
+++ migrations/m160516_084717_add_space_spot_id.php
@@ -1,34 +1,5 @@
 <?php
 
-use yii\db\Migration;
-
-class m160516_084717_add_space_spot_id extends Migration
-{
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m160516_084717_add_space_spot_id cannot be reverted.\n";
-
-        return false;
-    }
-
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
-
-    public function safeDown()
-    {
-    }
-    */
-}
-<?php
-
 use yii\db\Schema;
 use yii\db\Migration;
 
