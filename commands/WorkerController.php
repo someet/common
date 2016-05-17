979c753296826284621341e6c75d0b3df08cf7c1
wshudong <wsd312@163.com>
2016-05-17 17:58:09 +0800

    fix psuh templete bug

--- commands/WorkerController.php
+++ commands/WorkerController.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\commands;
 
-use common\models\Noti;
+use someet\common\models\Noti;
 use someet\common\models\Answer;
 use someet\common\models\User;
 use udokmeci\yii2beanstalk\BeanstalkController;
@@ -19,7 +19,8 @@ class WorkerController extends BeanstalkController
     const DELAY_MAX = 3;
 
     // sms 发送审核结果  wechat 发送审核结果 noti 发送活动前开始提醒 notiwechat 发送活动前开始提醒 checkinwechat 签到成功的微信通知
-    public function listenTubes(){
+    public function listenTubes()
+    {
         return ["sms", "wechat", "noti", "notiwechat", "wechatofficial"];
     }
 
@@ -33,7 +34,8 @@ class WorkerController extends BeanstalkController
      *                 self::NO_ACTION
      *                 self::DECAY
      */
-    public function actionSms($job){
+    public function actionSms($job)
+    {
         $sentData = $job->getData();
         try {
             $mobile = $sentData->mobile;
@@ -45,19 +47,22 @@ class WorkerController extends BeanstalkController
             $smsRes = $sms->sendSms($mobile, $smsData);
 
             //如果是未审核,则只修改发送时间
-            if ( $smsRes && in_array($answer->status, [Answer::STATUS_REVIEW_PASS, Answer::STATUS_REVIEW_REJECT])) {
+            if ($smsRes && in_array($answer->status, [Answer::STATUS_REVIEW_PASS, Answer::STATUS_REVIEW_REJECT])) {
                 //修改短信发送状态为成功, 以及修改发送时间
-                Answer::updateAll(['is_send' => Answer::STATUS_SMS_SUCC, 'send_at' => time()],
-                    ['id' => $answer->id]);
+                Answer::updateAll(
+                    ['is_send' => Answer::STATUS_SMS_SUCC, 'send_at' => time()],
+                    ['id' => $answer->id]
+                );
             } elseif ($sms->hasError()) {
-
                 $error = $sms->getError();
 
                 Yii::error('短信发送失败, 请检查'. is_array($error) ? json_encode($error) : $error);
 
                 //修改短信发送状态为失败, 以及修改发送时间[方便以后单独发送短信]
-                Answer::updateAll(['send_at' => time()],
-                    ['id' => $answer->id]);
+                Answer::updateAll(
+                    ['send_at' => time()],
+                    ['id' => $answer->id]
+                );
             }
 
             fwrite(STDOUT, Console::ansiFormat("Sms - Everything is allright"."\n", [Console::FG_GREEN]));
@@ -78,7 +83,8 @@ class WorkerController extends BeanstalkController
      *                 self::NO_ACTION
      *                 self::DECAY
      */
-    public function actionWechat($job){
+    public function actionWechat($job)
+    {
         $sentData = $job->getData();
         try {
             $templateData = (array) $sentData->templateData;
@@ -99,26 +105,23 @@ class WorkerController extends BeanstalkController
                 Answer::updateAll(['wechat_template_msg_id' => $msgid, 'wechat_template_is_send' => Answer::STATUS_WECHAT_TEMPLATE_SUCC, 'wechat_template_push_at' => time()], ['id' => $answer->id]);
 
                 // 审核成功后 用户通过次数加 1
-                if ($userJoinCount < $answerNum ) {
-                
-                    if($answer->status == Answer::STATUS_REVIEW_PASS){
-                        User::updateAllCounters(['attend_count' => 1],['id' => $user_id ]);
-                    }else if ($answer->status == Answer::STATUS_REVIEW_REJECT) {
-                        User::updateAllCounters(['reject_count' => 1],['id' => $user_id ]);
+                if ($userJoinCount < $answerNum) {
+                    if ($answer->status == Answer::STATUS_REVIEW_PASS) {
+                        User::updateAllCounters(['attend_count' => 1], ['id' => $user_id ]);
+                    } elseif ($answer->status == Answer::STATUS_REVIEW_REJECT) {
+                        User::updateAllCounters(['reject_count' => 1], ['id' => $user_id ]);
                     }
                 }
             } else {
-
                 //更新报名的模板消息发送的时间和状态, 状态为失败,后面可以单独的重新发送模板消息
                 Answer::updateAll(['wechat_template_is_send' => Answer::STATUS_WECHAT_TEMPLATE_Fail, 'wechat_template_push_at' => time()], ['id' => $answer->id]);
                 // 审核成功后 用户通过次数加 1
                 
-                if ($userJoinCount < $answerNum ) {
-                
-                    if($answer->status == Answer::STATUS_REVIEW_PASS){
-                        User::updateAllCounters(['attend_count' => 1],['id' => $user_id ]);
-                    }else if ($answer->status == Answer::STATUS_REVIEW_REJECT) {
-                        User::updateAllCounters(['reject_count' => 1],['id' => $user_id ]);
+                if ($userJoinCount < $answerNum) {
+                    if ($answer->status == Answer::STATUS_REVIEW_PASS) {
+                        User::updateAllCounters(['attend_count' => 1], ['id' => $user_id ]);
+                    } elseif ($answer->status == Answer::STATUS_REVIEW_REJECT) {
+                        User::updateAllCounters(['reject_count' => 1], ['id' => $user_id ]);
                     }
                 }
             }
@@ -140,7 +143,8 @@ class WorkerController extends BeanstalkController
      *                 self::DELETE
      *                 self::DECAY
      */
-    public function actionNoti($job) {
+    public function actionNoti($job)
+    {
         $sentData = $job->getData();
         try {
             $mobile = $sentData->mobile;
@@ -153,21 +157,23 @@ class WorkerController extends BeanstalkController
 
             if ($smsRes) {
                 //修改参加活动通知的短信发送状态为成功, 以及修改发送时间
-                Answer::updateAll(['join_noti_is_send' => Answer::JOIN_NOTI_IS_SEND_SUCC, 'join_noti_send_at' => time()],
-                    ['id' => $answer->id]);
+                Answer::updateAll(
+                    ['join_noti_is_send' => Answer::JOIN_NOTI_IS_SEND_SUCC, 'join_noti_send_at' => time()],
+                    ['id' => $answer->id]
+                );
             } elseif ($sms->hasError()) {
-
                 $error = $sms->getError();
                 Yii::error('短信发送失败, 请检查'. is_array($error) ? json_encode($error) : $error);
 
                 //修改短信发送状态为失败, 以及修改发送时间[方便以后单独发送短信]
-                Answer::updateAll(['join_noti_send_at' => time()],
-                    ['id' => $answer->id]);
+                Answer::updateAll(
+                    ['join_noti_send_at' => time()],
+                    ['id' => $answer->id]
+                );
             }
 
             fwrite(STDOUT, Console::ansiFormat("Noti - Everything is allright"."\n", [Console::FG_GREEN]));
             return self::DELETE;
-
         } catch (\Exception $e) {
             fwrite(STDERR, Console::ansiFormat($e."\n", [Console::FG_RED]));
             return self::BURY;
@@ -184,7 +190,8 @@ class WorkerController extends BeanstalkController
      *                 self::NO_ACTION
      *                 self::DECAY
      */
-    public function actionNotiwechat($job){
+    public function actionNotiwechat($job)
+    {
         $sentData = $job->getData();
         try {
             $templateData = (array) $sentData->templateData ;
@@ -198,7 +205,6 @@ class WorkerController extends BeanstalkController
                 //更新报名的模板消息的id, 发送的时间和状态
                 Answer::updateAll(['join_noti_wechat_template_msg_id' => $msgid, 'join_noti_wechat_template_is_send' => Answer::STATUS_WECHAT_TEMPLATE_SUCC, 'join_noti_wechat_template_push_at' => time()], ['id' => $answer->id]);
             } else {
-
                 //更新报名的模板消息发送的时间和状态, 状态为失败,后面可以单独的重新发送模板消息
                 Answer::updateAll(['join_noti_wechat_template_is_send' => Answer::STATUS_WECHAT_TEMPLATE_Fail, 'join_noti_wechat_template_push_at' => time()], ['id' => $answer->id]);
             }
@@ -222,17 +228,17 @@ class WorkerController extends BeanstalkController
      *                 self::DECAY
      *
      */
-    public function actionWechatofficial($job) {
+    public function actionWechatofficial($job)
+    {
         $sentData = $job->getData();
         try {
             $templateData =  $sentData->templateData ;
             $noti = $sentData->noti;
             $wechat = Yii::$app->wechat;
 
-            if ($msgid = $wechat->sendTemplateMessage(json_decode($templateData,true))) {
+            if ($msgid = $wechat->sendTemplateMessage(json_decode($templateData, true))) {
                 Noti::updateAll(['callback_id' => $msgid, 'callback_status' => Noti::CALLBACK_STATUS_SUCCESS, 'sended_at' => time()], ['id' => $noti->id]);
             } else {
-
                 Noti::updateAll(['callback_msg' => $msgid, 'callback_status' => Noti::CALLBACK_STATUS_FAILURE, 'sended_at' => time()], ['id' => $noti->id]);
             }
 
@@ -281,5 +287,4 @@ class WorkerController extends BeanstalkController
             return self::BURY;
         }
     }*/
-
 }
