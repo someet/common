7dced6780ac57ce3a9cc5ab85703ad69b2dfb271
maxwelldu <dcj3sjt@163.com>
2016-05-25 15:46:31 +0800

    add AnswerService

--- services/AnswerService.php
+++ services/AnswerService.php
@@ -5,6 +5,7 @@ use someet\common\models\Activity;
 use someet\common\models\QuestionItem;
 use someet\common\models\AnswerItem;
 use someet\common\models\Answer;
+use someet\common\models\User;
 use Yii;
 
 class AnswerService extends \someet\common\models\Answer
@@ -16,28 +17,64 @@ class AnswerService extends \someet\common\models\Answer
      *
      * @param int $question_id 问题ID
      * @param int $activity_id 活动ID
-     * @param int $user_id 用户ID
+     * @param array $post 提交的答案 For example:
+     * {
+     *      "question_id":"2",
+     *      "activity_id":"78",
+     *      "q1":{
+     *          "question_item_id":"76",
+     *          "question_value":"haha"
+     *      },
+     *      "q2":{
+     *          "question_item_id":"77",
+     *          "question_value":"hha"
+     *      },
+     *      "q3":{
+     *          "question_item_id":"78",
+     *          "question_value":"haa"
+     *      }
+     * }
      * @return array
      * @throws \yii\db\Exception
      */
     public function join($question_id, $activity_id, $post)
     {
         $user_id = Yii::$app->user->id;
+
+        if (!is_array($post['q1']) || !is_array($post['q2']) || !is_array($post['q3'])) {
+            $this->setError('三个问题不完整');
+            return false;
+        }
+
         $activity = Activity::findOne($activity_id);
-
         if (!$activity) {
-            return ['msg' => '活动不存在'];
+            $this->setError('活动不存在');
+            return false;
         }
 
-        //检查是否已经报过名
+        if ($activity->status == Activity::STATUS_SHUT) {
+            $this->setError('当前活动已关闭');
+            return false;
+        }
+
+        if ($activity->join_people_count >= $activity->peoples) {
+            $this->setError('活动已报满,无法报名');
+            return false;
+        }
+
         if (Answer::find()->where(['question_id' => $question_id, 'user_id' => $user_id])->exists()) {
-            return ['msg' => '无法重复报名'];
+            $this->setError('无法重新报名');
+            return false;
         }
 
-        $user = \someet\common\models\User::findOne($user_id);
+        $user = User::findOne($user_id);
 
         //获取问题列表
         $questionItemList = QuestionItem::findAll(['question_id' => $question_id]);
+        if (3 != count($questionItemList)) {
+            $this->setError('问题不是三个');
+            return false;
+        }
 
         // 组装answerlist
         $answerItemList = [
@@ -58,69 +95,54 @@ class AnswerService extends \someet\common\models\Answer
             ],
         ];
 
-        //创建主答案对象
         $model = new Answer();
-        //标记报名成功
-        $answerFlag = true;
-        //开启事务
         $transaction = $model->getDb()->beginTransaction();
 
-        //尝试保存答案主对象
-        $data = ['question_id' => $question_id, 'activity_id' => $activity_id, 'user_id' => $user_id, 'status' => Answer::STATUS_REVIEW_YET];
+        $data = ['question_id' => $question_id, 'activity_id' => $activity_id, 'status' => Answer::STATUS_REVIEW_YET];
         $model->load($data, '');
-        $model->save();
+        if (!$model->save()) {
+            $transaction->rollBack();
+            $this->setError('答案保存失败');
+            return false;
+        }
+
         foreach ($answerItemList as $answer) {
-            //创建答案项
             $answerModel = new AnswerItem();
-            $answerModel->question_item_id = $answer['question_item_id'];
-            $answerModel->question_value = $answer['question_value'];
             $answerModel->question_id = $question_id;
-            $answerModel->question_label = $answer['question_label'];
-            $answerModel->save();
-            //尝试保存答案项
-            if ($answerModel->load($answer, '') && $answerModel->save()) { //
-            } else {
-                $answerFlag = false;
+            if (!$answerModel->load($answer, '') || !$answerModel->save()) {
+                $transaction->rollBack();
+                $this->setError('答案项保存失败');
+                return false;
             }
         }
 
-        //更新用户的添加次数
-        if ($answerFlag && 0 == $user->updateCounters(['join_count' => +1])) {
-            $answerFlag = false;
+        if (0 == $user->updateCounters(['join_count' => 1])) {
+            $transaction->rollBack();
+            $this->setError('更新用户参加次数失败');
+            return false;
         }
 
-        //尝试更新允许报名的次数
-        if ($answerFlag && 0 == $user->updateCounters(['allow_join_times' => -1])) {
-            $answerFlag = false;
-        }
-
-        if ($answerFlag && 0 == Activity::updateAllCounters(['join_people_count' => +1], ['id' => $activity_id])) {
-            $answerFlag = false;
+        if (0 == $activity->updateCounters(['join_people_count' => 1])) {
+            $transaction->rollBack();
+            $this->setError('更新活动参加的人数失败');
+            return false;
         }
 
         //查询现在的活动人数是否已经报满
-        $join_people_count = Answer::find()
-            ->where(['activity_id' => $activity_id ])
-            ->count();
+        $join_people_count = $activity->join_people_count;
         $is_full = $join_people_count < $activity->peoples ? Activity::IS_FULL_NO : Activity::IS_FULL_YES;
 
         //如果 is_full 和之前的值一样则无需要更新
         if ($is_full != $activity->is_full) {
             //尝试更新活动是否已报名完成字段, updateAll 返回受影响的行数,如果修改成功一条则返回1, 如果修改失败则标识报名失败
-            if ($answerFlag &&  0 == Activity::updateAll(['is_full' => $is_full], ['id' => $activity_id])) {
-                $answerFlag = false;
+            if (0 == $activity->updateAll(['is_full' => $is_full])) {
+                $transaction->rollBack();
+                $this->setError('更新活动是否报满失败');
+                return false;
             }
         }
 
-        //如果报名成功
-        if ($answerFlag) {
-            //提交事务
-            $transaction->commit();
-            return ['报名成功'];
-        }
-
-        //事务回滚
-        $transaction->rollBack();
-        return ['msg'=>'报名失败，请重新报名'];
+        $transaction->commit();
+        return true;
     }
 }
