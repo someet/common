91d83df27ec1360a6a29a39bfa334437074ac7a2
wshudong <wsd312@163.com>
2016-05-31 20:27:58 +0800

    设置表单成功

---
+++ client/script/controllers/FounderQuestionCtrl.js
@@ -0,0 +1,69 @@
+angular.module('controllers').controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 'entity', '$questionManage', '$mdDialog', 'lodash',
+    '$mdToast',
+    function($scope, $uibModalInstance, entity, $questionManage, $mdDialog, lodash,
+        $mdToast) {
+        $scope.questionItem = {};
+        $questionManage.fetchByActivityId(entity.id).then(function(data) {
+            $scope.entity = data;
+            console.log($scope.entity);
+        }, function(err) {
+            alert(err);
+        });
+
+        // 设置问题
+        $scope.questionSave = function() {
+            var newEntity = {
+                activity_id: entity.id,
+                questionItemList: [],
+            };
+
+            if ($scope.entity != null && $scope.entity.id > 0) { // 更新
+                newEntity.id = $scope.entity.id;
+                for (var k in $scope.entity.questionItemList) {
+                    var questionItem = {
+                        question_id: newEntity.id,
+                        id: $scope.entity.questionItemList[k].id,
+                        label: $scope.entity.questionItemList[k].label,
+                    };
+                    newEntity.questionItemList.push(questionItem);
+                }
+                console.log(questionItem);
+                $questionManage.update($scope.entity.id, newEntity).then(function(data) {
+                    $mdToast.show($mdToast.simple()
+                        .content('问题保存成功')
+                        .hideDelay(5000)
+                        .position("top right"));
+                }, function(err) {
+                    alert(err);
+                });
+            } else { // 新建
+                var questionItem1 = {
+                    label: $scope.questionItem.q1,
+                };
+                var questionItem2 = {
+                    label: $scope.questionItem.q2,
+                };
+                var questionItem3 = {
+                    label: $scope.questionItem.q3,
+                };
+                newEntity.questionItemList.push(questionItem1);
+                newEntity.questionItemList.push(questionItem2);
+                newEntity.questionItemList.push(questionItem3);
+                $questionManage.create(newEntity).then(function(data) {
+                    $mdToast.show($mdToast.simple()
+                        .content('问题添加成功')
+                        .hideDelay(5000)
+                        .position("top right"));
+                }, function(err) {
+                    alert(err);
+                });
+            }
+            // 关闭摸态框
+            $uibModalInstance.close();
+        };
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }
+]);
