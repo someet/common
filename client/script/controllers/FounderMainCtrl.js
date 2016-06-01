1c32932b42424884a3759976bf1eaf59316a88d8
wshudong <wsd312@163.com>
2016-06-01 20:43:52 +0800

    表单复制成功

---
+++ client/script/controllers/FounderMainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('founderControllers')
+  .controller('MainCtrl', ['$scope', '$mdSidenav', '$location', function($scope, $mdSidenav, $location) {
+    $scope.toggleSidenav = function(menuId) {
+      $mdSidenav(menuId).toggle();
+    };
+
+    $scope.isActive = function(route) {
+      return $location.path().indexOf(route) != -1;
+    }
+  }]);
