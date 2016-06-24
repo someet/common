09d2f76c2073e1629e08ec478518c0d0b40e9b48
wshudong <wsd312@163.com>
2016-06-24 09:55:26 +0800

    mobule msg

--- models/MobileMsg.php
+++ models/MobileMsg.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace common\models;
+namespace someet\common\models;
 
 use Yii;
 
@@ -15,15 +15,18 @@ use Yii;
  * @property integer $activity_id
  * @property string $content
  * @property integer $is_join_queue
- * @property integer $is_join_queue_time
+ * @property integer $join_queue_at
  * @property integer $is_send
- * @property integer $is_send_time
- * @property integer $create_time
+ * @property integer $send_at
+ * @property integer $create_at
  * @property integer $msg_type
  * @property string $status
  */
 class MobileMsg extends \yii\db\ActiveRecord
 {
+
+    const STATUS_SMS_SUCC = 1;
+    const STATUS_SMS_YET = 0;
     /**
      * @inheritdoc
      */
@@ -38,7 +41,7 @@ class MobileMsg extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'mobile_ model', 'activity_id', 'is_join_queue', 'is_join_queue_time', 'is_send', 'is_send_time', 'create_time', 'msg_type'], 'integer'],
+            [['user_id', 'mobile_ model', 'activity_id', 'is_join_queue', 'join_queue_at', 'is_send', 'send_at', 'create_at', 'msg_type'], 'integer'],
             [['content'], 'string'],
             [['username', 'status'], 'string', 'max' => 255],
             [['mobile_num'], 'string', 'max' => 45]
@@ -59,10 +62,10 @@ class MobileMsg extends \yii\db\ActiveRecord
             'activity_id' => 'Activity ID',
             'content' => 'Content',
             'is_join_queue' => 'Is Join Queue',
-            'is_join_queue_time' => 'Is Join Queue Time',
+            'join_queue_at' => 'Join Queue At',
             'is_send' => 'Is Send',
-            'is_send_time' => 'Is Send Time',
-            'create_time' => 'Create Time',
+            'send_at' => 'Send At',
+            'create_at' => 'Create At',
             'msg_type' => 'Msg Type',
             'status' => 'Status',
         ];
