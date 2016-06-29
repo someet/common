1b32e1ce5c119276adf7dbcd38502b58bbf46202
wshudong <wsd312@163.com>
2016-06-29 18:29:31 +0800

    add migrations

---
+++ packages/someet-common/migrations/m160629_085631_add_channel_function.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160629_085631_add_channel_function extends Migration
+{
+    public function up()
+    {
+        $items = [
+            '/mobile/activity/channel-view' => ['user'],
+            '/mobile/activity/channel' => ['user'],
+            '/mobile/activity/channel-page' => ['user'],
+        ];
+
+        $authItemTemplate = <<<SQL
+INSERT INTO auth_item (name, type, description, rule_name, data, created_at, updated_at) VALUES ('%s', '2', '', null, null, null, null);
+SQL;
+        $itemChildTemplate = <<<SQL
+        INSERT INTO auth_item_child (parent, child) VALUES ('%s', '%s');
+SQL;
+        $sql = '';
+        foreach ($items as $item => $roles) {
+            $sql .= sprintf($authItemTemplate, $item);
+            foreach ($roles as $role) {
+                $sql .= sprintf($itemChildTemplate, $role, $item);
+            }
+        }
+        $this->execute($sql);
+        return true;
+    }
+
+    public function down()
+    {
+        echo "m160617_081711_add_check_change_status_permission cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
