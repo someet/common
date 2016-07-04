15d54fe70aefd71246b3ea8dc1eeca7ea1a23559
wshudong <wsd312@163.com>
2016-07-04 18:29:45 +0800

    jpush not found data

--- models/AppPush.php
+++ models/AppPush.php
@@ -14,9 +14,11 @@ use Yii;
  * @property string $from_type
  * @property integer $from_id
  * @property integer $from_status
- * @property integer $is_read
- * @property integer $is_push
  * @property integer $is_join_queue
+ * @property integer $join_at
+ * @property integer $is_push
+ * @property integer $is_read
+ * @property integer $push_at
  * @property integer $created_at
  * @property integer $status
  */
@@ -28,13 +30,14 @@ class AppPush extends \yii\db\ActiveRecord
     const IS_READ_NO = 0;
     // 队列发送成功
     const QUEUE_SEND_SUCC = 1;
-    // 队列发送失败
+    // 队列还未发送
     const QUEUE_SEND_YET = 0;
 
     // 加入队列成功
     const QUEUE_JOIN_SUCC = 1;
-    // 加入队列失败
+    // 加入还未加入队列
     const QUEUE_JOIN_YET = 0;
+
 
     /**
      * @inheritdoc
@@ -50,7 +53,7 @@ class AppPush extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'from_id', 'from_status', 'is_push','is_join_queue','is_read', 'created_at', 'status'], 'integer'],
+            [['user_id', 'from_id', 'from_status', 'is_join_queue', 'join_at', 'is_push', 'is_read', 'push_at', 'created_at', 'status'], 'integer'],
             [['content', 'from_type'], 'required'],
             [['jiguang_id', 'from_type'], 'string', 'max' => 64],
             [['content'], 'string', 'max' => 255],
@@ -64,17 +67,19 @@ class AppPush extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'user_id' => '用户编号',
-            'jiguang_id' => '极光编号',
-            'content' => '推送内容',
-            'from_type' => '内容来源类型，例如活动，用户等',
-            'from_id' => '来源编号，如果是活动的话，则是活动编号',
-            'from_status' => '来源的状态 例如活动的通过，不通过状态',
-            'is_push' => '是否推送',
-            'is_join_queue' => '是否加入队列',
-            'is_read' => '是否已读 0 未读 1 已读',
-            'created_at' => '推送时间',
-            'status' => '状态 ',
+            'user_id' => 'User ID',
+            'jiguang_id' => 'Jiguang ID',
+            'content' => 'Content',
+            'from_type' => 'From Type',
+            'from_id' => 'From ID',
+            'from_status' => 'From Status',
+            'is_join_queue' => 'Is Join Queue',
+            'join_at' => 'Join At',
+            'is_push' => 'Is Push',
+            'is_read' => 'Is Read',
+            'push_at' => 'Push At',
+            'created_at' => 'Created At',
+            'status' => 'Status',
         ];
     }
 
@@ -87,5 +92,4 @@ class AppPush extends \yii\db\ActiveRecord
 
         return $fields;
     }
-
 }
