06f9d61a10d7ee3c26f73c88211d5b5ba29e2a4c
wshudong <wsd312@163.com>
2016-07-05 16:06:51 +0800

    add jiguang

--- services/AppPushService.php
+++ services/AppPushService.php
@@ -1,13 +1,15 @@
+<!--
+@Author: stark <wangshudong>
+@Date:   2016-07-04T13:34:07+08:00
+@Email:  wsd312@163.com
+@Last modified by:   wangshudong
+@Last modified time: 2016-07-05T15:31:28+08:00
+-->
+
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: maxwelldu
- * Date: 28/6/2016
- * Time: 6:18 PM
- */
-
 namespace someet\common\services;
-
 
 use someet\common\models\AppPush;
 use Yii;
@@ -28,23 +30,36 @@ class AppPushService extends BaseService
     }
 
     /**
-    * 极光推送服务
-    */
-    public static function jpush($id, $content)
+     * 极光推送
+     * @param  integer $jpush 极光推送数据
+     * @return bool    推送是否成功
+     */
+    public static function jpush($jpush)
     {
-        //发送push
+        // 当数据为空时返回值为0
+        if(empty($jpush)){
+            return AppPush::QUEUE_SEND_YET;
+        }
+        //推送
         $res = Yii::$app->jpush->push()
                 ->setPlatform(['ios', 'android'])
                 // ->addAllAudience()
-                ->addRegistrationId([$id])
-                    //->addTag(['北京'])
+                ->addRegistrationId([$jpush->jiguang_id])
+                //->addTag(['北京'])
                 //->addAlias('alias1')
-                        // ->addAndroidNotification('Hi, android notification', 'notification title', 1, ['key1' => 'value1', 'key2' => 'value2'])
-                    ->addIosNotification($content, null, "+1", true, "ios category", ['key1' => 'value1'])
+                // ->addAndroidNotification('Hi, android notification', 'notification title', 1, ['key1' => 'value1', 'key2' => 'value2'])
+                ->addIosNotification($jpush->content, null, "+1", true, "ios category", [
+                        'from_type' => $jpush->from_type,
+                        'from_id' => $jpush->from_id,
+                        'from_status' => $jpush->from_status,
+                        'push_at' => $jpush->push_at,
+                ])
                 ->setNotificationAlert('test'.date('Y-m-d H:i:s', time()))
                 ->send();
-
-        return $res;
+        if ($res) {
+            echo 'Result=' . json_encode($res)."\n";
+            return AppPush::QUEUE_SEND_SUCC;
+        }
     }
 
 }
