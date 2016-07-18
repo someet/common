152b06dbdd009ec85ced77e8749af62a45a28e8a
maxwelldu <dcj3sjt@163.com>
2016-07-18 17:16:33 +0800

    add city manage

---
+++ models/City.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace someet\common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $id
+ * @property string $city
+ * @property integer $city_id
+ * @property integer $status
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['city', 'city_id'], 'required'],
+            [['city_id', 'status'], 'integer'],
+            [['city'], 'string', 'max' => 60],
+            [['city'], 'unique'],
+            [['city_id'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'city' => 'City',
+            'city_id' => 'City ID',
+            'status' => 'Status',
+        ];
+    }
+}
