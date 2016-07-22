45a23501a14b40c793e4a309caa15a4e42ff7b65
wshudong <wsd312@163.com>
2016-07-22 17:50:33 +0800

    init

--- services/EventService.php
+++ services/EventService.php
@@ -28,7 +28,7 @@ class EventService extends BaseService
      * @param  init $activity_id 活动id
      * @return 是否执行成功
      */
-    public function applyAfter($activity_id)
+    public static function applyAfter($activity_id)
     {
         // 更新活动是否报满
         AnswerService::updateIsfull($activity_id);
@@ -42,7 +42,7 @@ class EventService extends BaseService
      * @param  init $activity_id 活动id
      * @return 是否执行成功
      */
-    public function applyBefore()
+    public static function applyBefore()
     {
         /*
         报名前执行的事件 检测
@@ -58,23 +58,42 @@ class EventService extends BaseService
      * 前台：pma和发起人筛选执行的事件
      * @return 是否执行成功
      */
-    public function managerFilter()
+    public static function managerFilter()
     {
-    }    
+    }
 
     /**
      * 后台：报名名额改动
      * @return 是否执行成功
      */
-    public function managerFilter()
+    public static function applyLimit($activity_id)
     {
-    }    
+        // 更新活动是否报满
+        AnswerService::updateIsfull($activity_id);
+
+        // 更新活动的报名率
+        ActivityService::updateRepalyRate($activity_id);
+
+        //查询现在的活动人数是否已经报满
+        $activity = Activity::findOne($id);
+        $is_full = $activity->join_people_count < $data['peoples'] ? Activity::IS_FULL_NO : Activity::IS_FULL_YES;
+
+        //尝试更新活动是否已报名完成字段
+        //如果 is_full 和之前的值一样则无需要更新
+        if ($is_full != $activity->is_full) {
+            //如果活动更新成功，更新活动当更新成功返回0,所以取反表示活动更新成功
+            if (0 == Activity::updateAll(['is_full' => $is_full], ['id' => $id])) {
+                //更新错误
+                throw new DataValidationFailedException('更新活动数量失败');
+            }
+        }
+    }
 
     /**
      * 后台：理想人数改动
      * @return 是否执行成功
      */
-    public function managerFilter()
+    public static function idealLimit()
     {
     }
 }
