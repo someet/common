4700603807caabbe92e2e339fb7ec76c9881bc64
wshudong <wsd312@163.com>
2016-07-22 18:12:47 +0800

    add change peoples event

--- services/EventService.php
+++ services/EventService.php
@@ -68,25 +68,11 @@ class EventService extends BaseService
      */
     public static function applyLimit($activity_id)
     {
-        // 更新活动是否报满
+        // 更新活动是否报满字段
         AnswerService::updateIsfull($activity_id);
 
         // 更新活动的报名率
         ActivityService::updateRepalyRate($activity_id);
-
-        //查询现在的活动人数是否已经报满
-        $activity = Activity::findOne($id);
-        $is_full = $activity->join_people_count < $data['peoples'] ? Activity::IS_FULL_NO : Activity::IS_FULL_YES;
-
-        //尝试更新活动是否已报名完成字段
-        //如果 is_full 和之前的值一样则无需要更新
-        if ($is_full != $activity->is_full) {
-            //如果活动更新成功，更新活动当更新成功返回0,所以取反表示活动更新成功
-            if (0 == Activity::updateAll(['is_full' => $is_full], ['id' => $id])) {
-                //更新错误
-                throw new DataValidationFailedException('更新活动数量失败');
-            }
-        }
     }
 
     /**
@@ -95,5 +81,7 @@ class EventService extends BaseService
      */
     public static function idealLimit()
     {
+        // 更新活动是否报满字段
+        AnswerService::updateIsfull($activity_id);
     }
 }
